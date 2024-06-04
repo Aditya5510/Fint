09c06317ef1e3cf958e93e93f95d96e2ca29f30d
Aditya <swaroopaditya545@gmail.com>
2024-06-04 22:33:19 +0530

    using hono instead of default nextapi

--- app/(dash)/page.tsx
+++ app/(dash)/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const Home = () => {
   return (
     <div>
-      <Button>Home</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
